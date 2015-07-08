c7b057a230f730ef3f22c6c1b3dd4c581867c15f
Stefan Popp <mail@stefanpopp.de>
2015-07-08 19:54:01 +0200

    Initial Commit

---
+++ MarioMeetsStatusBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarioMeetsStatusBar
+//
+//  Created by Stefan Popp on 08.07.15.
+//  Copyright (c) 2015 SwiftBlog. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MarioMeetsStatusBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MarioMeetsStatusBar
+//
+//  Created by Stefan Popp on 08.07.15.
+//  Copyright (c) 2015 SwiftBlog. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
