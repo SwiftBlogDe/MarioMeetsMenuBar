2994bb310ef8ff2908635fe07e3af10d74cf2b6c
Stefan Popp <mail@stefanpopp.de>
2015-07-10 15:55:40 +0200

    Reduziertes Storyboard. Umstrukturierung, Kommentare, Animationen, Bridging zu Obj-c.de

--- MarioMeetsStatusBar/AppDelegate.swift
+++ MarioMeetsStatusBar/AppDelegate.swift
@@ -5,22 +5,96 @@
 //  Created by Stefan Popp on 08.07.15.
 //  Copyright (c) 2015 SwiftBlog. All rights reserved.
 //
+/*
+Todo:
+
+Screen change
+Screen resolution change
+Animationsperformance optimieren
+
+Nice to have:
+Animation ein wenig verspielter machen
+*/
 
 import Cocoa
 
+class MarioWindow: NSWindow {
+    override func constrainFrameRect(frameRect: NSRect, toScreen screen: NSScreen?) -> NSRect {
+        return frameRect
+    }
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    var window: MarioWindow!
+    var marioController: MarioWindowController!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-    }
+        
+        // Verhindere die Anzeige der Applikation im Dock
+        NSApp.setActivationPolicy(NSApplicationActivationPolicy.Accessory)
+        
+        // Erstelle das Anwendungsfenster und zeige es an
+        createApplicationWindow()
 
+        // Mario controller erstellen und loslaufen lasse
+        addMarioController()
+        
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func addMarioController() {
+        // Erstelle Mario und lass ihn laufen
+        marioController = MarioWindowController(marioWindow: window)
+        marioController.start()
+    }
+    
+    func destroyApplicationWindow() {
+        window.close()
+    }
+    
+    func createApplicationWindow() {
+        // Aufloesung abfragen und Bereich zum zeichnen abfragen
+        let screenFrame = NSScreen.mainScreen()!.frame
+        let marioFrame = MarioWindowController.marioFrame
+        let contentRect = CGRect(
+            x: 0,
+            y: screenFrame.height-marioFrame.height,
+            width: screenFrame.size.width,
+            height: marioFrame.height
+        )
+        
+        // Erstelle Mario Fenster und platziere es ueber alle anderen Fenster
+        window = MarioWindow(
+            contentRect: contentRect, // Fensterposition
+            styleMask: NSBorderlessWindowMask, // Keine Titlebar und Rand
+            backing: NSBackingStoreType.Buffered,
+            defer: true
+        )
+        window.hasShadow = false
+        window.level = Int(CGWindowLevelForKey(Int32(kCGMainMenuWindowLevelKey)))
+        window.opaque = false
+        window.backgroundColor = NSColor(
+            calibratedHue: 0,
+            saturation: 1.0,
+            brightness: 0,
+            alpha: 0.0
+        )
+        
+        // Jegliche Maus-Events unterdruecken
+        window.ignoresMouseEvents = true
+        
+        // Applikationsfenster nach vorne holen, dabei den Fokus anderer 
+        // Applikationen unangetastet lassen
+        window.orderFrontRegardless()
+        
+        // Erstellen eines Platzhalter Views und Zuweisung an das Window
+        let contentView = NSView(frame: contentRect)
+        window.contentView = contentView
+    }
 }
 
--- MarioMeetsStatusBar/MarioController.swift
+++ MarioMeetsStatusBar/MarioController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MarioController.swift
 //  MarioMeetsStatusBar
 //
 //  Created by Stefan Popp on 08.07.15.
@@ -8,20 +8,105 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+// Versatz den Mario ausserhalb des Bildschirms noch links und rechts laeuft
+let kMarioOffscreenOffset: CGFloat = 128.0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+// Subclass wenn mal etwas Fanciness dazu kommen soll =)
+class MarioImageView: NSImageView {
+    
+}
 
-        // Do any additional setup after loading the view.
+class MarioWindowController {
+    
+    // Hauptfenster
+    let marioWindow: MarioWindow!
+    
+    // Marios Frame in dem wir zeichnen
+    static let marioFrame = CGRect(
+        x: -kMarioOffscreenOffset,
+        y: 0.0,
+        width: 22.0,
+        height: 22.0
+    )
+    
+    // Marios Image View
+    let marioImageView: MarioImageView!
+    
+    // Erstellen des Image views und erzeugen von Layern zur animierung
+    init(marioWindow: MarioWindow) {
+        self.marioWindow = marioWindow
+        marioImageView = MarioImageView(frame: MarioWindowController.marioFrame)
+        marioImageView.layer = CALayer()
+        marioImageView.layerContentsRedrawPolicy = NSViewLayerContentsRedrawPolicy.OnSetNeedsDisplay;
+        marioImageView.wantsLayer = true
+    }
+    
+    func start() {
+        // Mario Image View hinzufuegen
+        addMario()
+        
+        // Mario animation starten
+        animateMario()
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    func animateMario() {
+        // Target X Koordinate errechnen
+        let targetX = self.marioImageView.superview!.frame.width + kMarioOffscreenOffset;
+        
+        // Laufanimation hinzufuegen (Sprites)
+        marioImageView.layer?.addAnimation(marioWalkSpriteAnimation(), forKey: "marioWalk")
+        
+        // Animation fuer die Verschiebung des Frames hinzufuegen
+        NSAnimationContext.runAnimationGroup({ (context: NSAnimationContext!) -> Void in
+            
+            // 23 - https://de.wikipedia.org/wiki/Dreiundzwanzig
+            context.duration = 23.0;
+            
+            // Linearer Zeitverlauf fuer die Animation
+            context.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+            
+            // Zielkoordinate festlegen
+            self.marioImageView.animator().frame = CGRectOffset(self.marioImageView.frame, targetX, 0);
+            }, completionHandler: { // Aufruf nach Beendigung der Animation
+                // Original Frame wiederherstellen
+                self.marioImageView.frame = MarioWindowController.marioFrame
+                
+                // Animation nach 4 Sekunden erneut starten
+                let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(4 * Double(NSEC_PER_SEC)))
+                dispatch_after(delayTime, dispatch_get_main_queue()) {
+                    self.animateMario()
+                }
+        })
     }
+    
+    func marioWalkSpriteAnimation() -> CAKeyframeAnimation {
 
-
+        // Keypath fuer den Austausch ist bei Image Views "contents"
+        let animation = CAKeyframeAnimation(keyPath: "contents")
+        
+        // Sprites als Einzelbilder
+        animation.values = [
+            NSImage(named: "mario1")!,
+            NSImage(named: "mario3")!,
+        ]
+        
+        // 500ms fuer einen Durchlauf der Animation
+        animation.duration = 0.5
+        
+        // Diskreter Zeitverlauf, linear uberblendet die Bilder
+        animation.calculationMode = kCAAnimationDiscrete
+        
+        // Wir wiederholen die Animation bis zum Sant Nimmermehrstag
+        // https://de.wikipedia.org/wiki/Sankt_Nimmerlein
+        animation.repeatCount = HUGE
+        
+        // Rueckgabe der erzeugten Animation
+        return animation
+    }
+    
+    func addMario() {
+        // Add mario image view to the main window
+        marioWindow.contentView.addSubview(marioImageView)
+    }
 }
 
